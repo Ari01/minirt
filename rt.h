9d7ab6176c640ac4cafe9dcce473407875d7589f
denden <denis.c1@live.fr>
2020-12-15 20:53:49 +0100

    minirt2 debuggage diffusion en cours

--- rt.h
+++ rt.h
@@ -1,48 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   types.h                                            :+:      :+:    :+:   */
+/*   rt.h                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/14 18:14:41 by user42            #+#    #+#             */
-/*   Updated: 2020/12/14 18:39:52 by user42           ###   ########.fr       */
+/*   Created: 2020/12/15 15:30:00 by user42            #+#    #+#             */
+/*   Updated: 2020/12/15 18:40:01 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef TYPES_H
-# define TYPES_H
+#ifndef RT_H
+# define RT_H
 
-/*
-** MLX
-*/
-typedef struct	s_img
-{
-	void		*img;
-	char		*addr;
-	int			bits_per_pixel;
-	int			line_length;
-	int			endian;
-}				t_img;
+# include "mlx.h"
+# include <math.h>
+# include <stdlib.h>
 
-typedef struct	s_rt
-{
-	void		*mlx;
-	void		*window;
-	t_img		img;
-	int			width;
-	int			height;
-}				t_rt;
-
-/*
-** COLOR AND GEOMETRIC OBJECTS
-*/
-typedef struct	s_color
-{
-	double	red;
-	double	green;
-	double	blue;
-}				t_color;
+# define min(x,y)	((x) < (y) ? (x) : (y))
 
 typedef struct	s_vector
 {
@@ -57,21 +32,45 @@ typedef struct	s_ray
 	t_vector dir;
 }				t_ray;
 
+typedef struct	s_color
+{
+	double r;
+	double g;
+	double b;
+}				t_color;
+
 typedef struct	s_sphere
 {
 	t_vector	pos;
-	t_color		color;
 	double		radius;
+	t_color		color;
 }				t_sphere;
 
-/*
-** CAM, LIGHT, HOLDER
-*/
-typedef struct	s_camera
+typedef	struct	s_viewport
 {
-	t_ray	viewray;
-	int		fov;
-}				t_camera;
+	int		width;
+	int		height;
+	double	distance;
+}				t_viewport;
+
+typedef struct	s_img
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_img;
+
+typedef struct	s_rt
+{
+	void		*mlx;
+	void		*window;
+	t_img		img;
+	int			width;
+	int			height;
+	t_viewport	viewport;
+}				t_rt;
 
 typedef struct	s_light
 {
@@ -79,13 +78,5 @@ typedef struct	s_light
 	t_color		color;
 	double		intensity;
 }				t_light;
-
-typedef struct	s_holder
-{
-	t_light		*light;
-	t_sphere	*sphere;
-	t_camera	*camera;
-	t_color		current_color;
-}				t_holder;
 
 #endif
