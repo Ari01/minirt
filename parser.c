8c27feeb547b35b9ea84adb380a1ec3c8a921bb6
denden <denis.c1@live.fr>
2021-01-01 10:51:54 +0100

    object parsing en cours

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/26 08:00:51 by user42            #+#    #+#             */
-/*   Updated: 2020/12/27 12:04:20 by user42           ###   ########.fr       */
+/*   Updated: 2021/01/01 10:51:19 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,16 @@ void	string_array_free(char **split)
 	int i;
 
 	i = 0;
-	while (split[i])
+	if (split)
 	{
-		free(split[i]);
-		i++;
+		while (split[i])
+		{
+			free(split[i]);
+			i++;
+		}
+		free(split);
+		split = NULL;
 	}
-	free(split);
-	split = NULL;
 }
 
 void	check_missing_parameters(t_rt *rt)
@@ -40,22 +43,34 @@ void	check_missing_parameters(t_rt *rt)
 
 int		set_color(char **split, t_color *color)
 {
+	int valid_format;
+
+	valid_format = 1;
 	if (!split || !split[0] || !split[1] || !split[2] || split[3])
-		return (0);
-	*color = new_color(ft_atod(split[0]), ft_atod(split[1]), ft_atod(split[2]));
-	if (color->r < 0 || color->r > 255
-		|| color->g < 0 || color->g > 255
-		|| color->b < 0 || color->b > 255)
-		return (0);
-	return (1);
+		valid_format = 0;
+	else
+	{
+		*color = new_color(ft_atod(split[0]), ft_atod(split[1]), ft_atod(split[2]));
+		if (color->r < 0 || color->r > 255
+			|| color->g < 0 || color->g > 255
+			|| color->b < 0 || color->b > 255)
+			valid_format = 0;
+	}
+	string_array_free(split);
+	return (valid_format);
 }
 
 int		set_coord(char **split, t_vector *coord)
 {
+	int	valid_format;
+
+	valid_format = 1;
 	if (!split || !split[0] || !split[1] || !split[2] || split[3])
-		return (0);
-	*coord = new_vector(ft_atod(split[0]), ft_atod(split[1]), ft_atod(split[2]));
-	return (1);
+		valid_format = 0;
+	else
+		*coord = new_vector(ft_atod(split[0]), ft_atod(split[1]), ft_atod(split[2]));
+	string_array_free(split);
+	return (valid_format);
 }
 
 int		correct_direction(t_vector dir)
@@ -69,6 +84,8 @@ int		correct_direction(t_vector dir)
 
 void	set_resolution(char **split, t_rt *rt)
 {
+	if (rt->width != -1 || rt->height != -1)
+		print_error_msg("resolution 'R' defined twice");
 	if (!split[1] || !split[2] || split[3])
 	{
 		string_array_free(split);
@@ -85,25 +102,22 @@ void	set_resolution(char **split, t_rt *rt)
 
 void	set_ambiant_light(char **split, t_rt *rt)
 {
-	char **tmp;
+	char			**color;
+	t_ambiant_light	ambiant_light;
 
+	if (rt->scene.ambiant_light.intensity != -1)
+		print_error_msg("ambiant light 'A' defined twice");
 	if (!split[1] || !split[2] || split[3])
 		print_error_msg("invalid ambiant light format");
-	tmp = ft_split(split[2], ",");
-	if (!set_color(tmp, &rt->scene.ambiant_light.color))
-	{
-		string_array_free(tmp);
-		string_array_free(split);
-		print_error_msg("invalid ambiant light color");
-	}
-	free(tmp);
-	rt->scene.ambiant_light.intensity = ft_atod(split[1]);
-	if (rt->scene.ambiant_light.intensity < 0
-		|| rt->scene.ambiant_light.intensity > 1)
+	color = ft_split(split[2], ",");
+	ambiant_light.intensity = ft_atod(split[1]);
+	if (!set_color(color, &ambiant_light.color) || ambiant_light.intensity < 0
+		|| ambiant_light.intensity > 1)
 	{
 		string_array_free(split);
 		print_error_msg("invalid ambiant light intensity");
 	}
+	rt->scene.ambiant_light = ambiant_light;
 }
 
 void	set_camera(char **split, t_rt *rt)
@@ -111,9 +125,7 @@ void	set_camera(char **split, t_rt *rt)
 	char		**coord;
 	char		**direction;
 	t_camera	camera;
-	int			invalid_coord;
 
-	invalid_coord = 0;
 	if (!split[1] || !split[2] || !split[3] || split[4])
 	{
 		string_array_free(split);
@@ -121,16 +133,13 @@ void	set_camera(char **split, t_rt *rt)
 	}
 	coord = ft_split(split[1], ",");
 	direction = ft_split(split[2], ",");
-	if (!set_coord(coord, &camera.position) || !set_coord(direction, &camera.direction)
-		|| !correct_direction(camera.direction))
-		invalid_coord = 1;
-	string_array_free(coord);
-	string_array_free(direction);
 	camera.fov = ft_atod(split[3]);
-	if (invalid_coord || camera.fov < 0 || camera.fov > 180)
+	printf("direction = %s %s %s\n", direction[0], direction[1], direction[2]);
+	if (!set_coord(coord, &camera.position) || !set_coord(direction, &camera.direction)
+		|| !correct_direction(camera.direction) || camera.fov < 0 || camera.fov > 180)
 	{
 		string_array_free(split);
-		print_error_msg("invalid camera fov");
+		print_error_msg("invalid camera parameter");
 	}
 	ft_lstadd_front(&rt->scene.camera, ft_lstnew(&camera));
 }
@@ -140,9 +149,7 @@ void	set_light(char **split, t_rt *rt)
 	char		**coord;
 	char		**color;
 	t_light		light;
-	int			invalid_param;
 
-	invalid_param = 0;
 	if (!split[1] || !split[2] || !split[3] || split[4])
 	{
 		string_array_free(split);
@@ -150,17 +157,85 @@ void	set_light(char **split, t_rt *rt)
 	}
 	coord = ft_split(split[1], ",");
 	color = ft_split(split[3], ",");
-	if (!set_coord(coord, &light.position) || !set_color(color, &light.color))
-		invalid_param = 1;
-	string_array_free(coord);
-	string_array_free(color);
 	light.intensity = ft_atod(split[2]);
-	if (invalid_param || light.intensity < 0 || light.intensity > 1)
+	if (!set_coord(coord, &light.position) || !set_color(color, &light.color)
+		|| light.intensity < 0 || light.intensity > 1)
 	{
 		string_array_free(split);
 		print_error_msg("invalid light parameters");
 	}
 	ft_lstadd_front(&rt->scene.light, ft_lstnew(&light));
+}
+
+void	set_sphere(char **split, t_rt *rt)
+{
+	char		**coord;
+	char		**color;
+	t_sphere	sphere;
+
+	if (!split[1] || !split[2] || !split[3] || split[4])
+	{
+		string_array_free(split);
+		print_error_msg("invalid sphere format");
+	}
+	coord = ft_split(split[1], ",");
+	color = ft_split(split[3], ",");
+	sphere.diameter = ft_atod(split[2]);
+	if (!set_coord(coord, &sphere.center) || !set_color(color, &sphere.color) || 
+		sphere.diameter <= 0)
+	{
+		string_array_free(split);
+		print_error_msg("invalid sphere parameters");
+	}
+	ft_lstadd_front(&rt->scene.objects, ft_lstnew(&sphere));
+}
+
+void	set_plane(char **split, t_rt *rt)
+{
+	char		**coord;
+	char		**dir;
+	char		**color;
+	t_plane		plane;
+
+	if (!split[1] || !split[2] || !split[3] || split[4])
+	{
+		string_array_free(split);
+		print_error_msg("invalid plane format");
+	}
+	coord = ft_split(split[1], ",");
+	dir = ft_split(split[2], ",");
+	color = ft_split(split[3], ",");
+	if (!set_coord(coord, &plane.position) || !set_coord(dir, &plane.direction) ||
+		!set_color(color, &plane.color) || !correct_direction(plane.direction))
+	{
+		string_array_free(split);
+		print_error_msg("invalid plane parameters");
+	}
+	ft_lstadd_front(&rt->scene.objects, ft_lstnew(&plane));
+}
+
+void	set_square(char **split, t_rt *rt)
+{
+	char		**coord;
+	char		**dir;
+	char		**color;
+	t_square	square;
+
+	if (!split[1] || !split[2] || !split[3] || split[4])
+	{
+		string_array_free(split);
+		print_error_msg("invalid plane format");
+	}
+	coord = ft_split(split[1], ",");
+	dir = ft_split(split[2], ",");
+	color = ft_split(split[3], ",");
+	if (!set_coord(coord, &plane.position) || !set_coord(dir, &plane.direction) ||
+		!set_color(color, &plane.color) || !correct_direction(plane.direction))
+	{
+		string_array_free(split);
+		print_error_msg("invalid plane parameters");
+	}
+	ft_lstadd_front(&rt->scene.objects, ft_lstnew(&plane));
 }
 
 t_rt	parse(char *pathfile)
@@ -212,8 +287,32 @@ t_rt	parse(char *pathfile)
 				light = *(t_light*)rt.scene.light->content;
 				printf("light coord = %f %f %f, intensity = %f, color = %f %f %f\n", light.position.x, light.position.y, light.position.z, light.intensity, light.color.r, light.color.g, light.color.b);
 			}
-			/*if (**line == 's' && **(line + 1) == 'p')
-				set_sphere(split, &rt);*/
+			if (!ft_strncmp(split[0], "sp", id_len))
+			{
+				set_sphere(split, &rt);
+				t_sphere sphere;
+				sphere = *(t_sphere*)rt.scene.objects->content;
+				printf("sphere coord = %f %f %f, diameter = %f, color = %f %f %f\n", sphere.center.x, sphere.center.y, sphere.center.z, sphere.diameter, sphere.color.r, sphere.color.g, sphere.color.b);
+			}
+			if (!ft_strncmp(split[0], "pl", id_len))
+			{
+				set_plane(split, &rt);
+				t_plane plane;
+				plane = *(t_plane*)rt.scene.objects->content;
+				printf("plane coord = %f %f %f, direction = %f %f %f, color = %f %f %f\n", plane.position.x, plane.position.y, plane.position.z, plane.direction.x, plane.direction.y, plane.direction.z, plane.color.r, plane.color.g, plane.color.b);
+			}
+			if (!ft_strncmp(split[0], "sq", id_len))
+			{
+				set_square(split, &rt);
+			}
+			if (!ft_strncmp(split[0], "cy", id_len))
+			{
+				set_cylindre(split, &rt);
+			}
+			if (!ft_strncmp(split[0], "tr", id_len))
+			{
+				set_triangle(split, &rt);
+			}
 			string_array_free(split);
 			free(line);
 			split = NULL;
